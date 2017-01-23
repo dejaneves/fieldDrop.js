3c6d45be057de1ca8b7a1a46d66ec8d0d647c109
Jaime Neves <jaimebarrosoneves@gmail.com>
2017-01-22 20:34:27 -0400

    inserting upload route

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const multer = require('multer');
+const app = express();
+
+const client = '/';
+const port = process.env.PORT || 8081;
+app.use(serveStatic(__dirname + client));
+
+app.listen(port,function(){
+  console.log("http://localhost:" + port);
+});
+
+
+/**
+ * Uploads
+ */
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    var ext = file.originalname.substr(file.originalname.lastIndexOf('.') + 1);
+    cb(null, file.fieldname + '-' + Date.now() + '.' + ext);
+  }
+}); const upload = multer({ storage: storage });
+
+// Route
+app.post('/upload', upload.single('testFile'), function (req, res, next) {
+  console.log(req.file);
+});
